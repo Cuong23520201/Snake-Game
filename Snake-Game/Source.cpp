c08c71e77edd214ea1ffee6376e6643836bb141c
HungCuong <23520201@gm.uit.edu.vn>
2024-11-09 16:38:39 +0700

    Khoi tao project

---
+++ Snake-Game/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+#include <chrono>
+#include <thread>
+#include <windows.h> 
+using namespace std;
+
+
+int main()
+{
+    run();
+    return 0;
+}
+
+void clearScreen() {
+    system("cls");
+}
+
+void run()
+{
+
+}
